5c6facbe13790696c429f2f76515b98de0f9a842
pothagoniharish <146047342+pothagoniharish@users.noreply.github.com>
2025-08-25 16:53:54 -0500

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "harish-pothagoni-bucket"
     region         = "us-east-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "my-dev-proj-01"
     encrypt        = true
   }
   required_version = ">=0.13.0"
